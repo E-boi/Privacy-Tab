24a97607a32588d4360ef7d47799c139c5a3b384
EpicPerson <54505527+metoo67789@users.noreply.github.com>
2021-03-01 19:34:01 -0500

    custom keybinds!
    custom keybinds and blur timing now added! mostly fixed bug where image or any pop up wouldn't blur

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const { Plugin } = require('powercord/entities');
-const Settings = require("./Settings")
+const Settings = require("./Components/Settings")
 var privacyEnabled = false;
+let keybind;
 
 module.exports = class PrivacyTab extends Plugin {
 	startPlugin() {
 		this.loadStylesheet('style.scss')
-		const settings = powercord.api.settings._fluxProps('Privacy-Tab')
-		settings.getSetting('blur-scale', 1)
-		settings.getSetting('grayscale', false)
-		settings.getSetting('lock-app', true)
+		const settings = powercord.api.settings._fluxProps('Privacy-Tab');
+		settings.getSetting('blur-scale', 1);
+		settings.getSetting('grayscale', false);
+		settings.getSetting('lock-app', true);
+		settings.getSetting('blurTiming', 1);
+		keybind = settings.getSetting('keybind', 'F6');
 
 		// init
-		document.onkeyup = this.togglePrivacy;
+		document.body.addEventListener("keyup", this.togglePrivacy);
 
 		// register settings
 		powercord.api.settings.registerSettings(this.entityID, {
@@ -23,6 +26,7 @@ module.exports = class PrivacyTab extends Plugin {
 
 	pluginWillUnload() {
 		powercord.api.settings.unregisterSettings(this.entityID);
+		document.body.removeEventListener("keyup", this.togglePrivacy);
 		if (privacyEnabled) {
 			document.querySelector('.layers-3iHuyZ').classList.remove('blur-window');
 		}
@@ -31,12 +35,12 @@ module.exports = class PrivacyTab extends Plugin {
 	// toggle for privacy
 	togglePrivacy(key) {
 		const { getSetting: get } = powercord.api.settings._fluxProps('Privacy-Tab')
-		const blurElement = document.querySelector('.layers-3iHuyZ');
+		const blurElement = document.querySelector('.appMount-3lHmkl');
 		// user safety #1 :P
 		if (!document.hasFocus()) {
 			return;
 		}
-		if (key.key === 'F6') {
+		if (key.key.toUpperCase() === keybind) {
 			privacyEnabled = !privacyEnabled;
 			if (privacyEnabled) {
 				// enable blur
@@ -48,9 +52,12 @@ module.exports = class PrivacyTab extends Plugin {
 				if (get("lock-app")) {
 					interaction = "none";
 				}
+				let timing = '1s';
+				if (get('blurTiming')) timing = `${get('blurTiming')}s`
 				const blurAmount = get("blur-scale") * 3;
 				blurElement.style.setProperty('--blur-window', `blur(${blurAmount}px) grayscale(${grayscale})`);
 				blurElement.style.setProperty('--pointer', `${interaction}`);
+				blurElement.style.setProperty('--blur-timing', `${timing}`);
 				blurElement.classList.add('blur-window');
 			} else {
 				blurElement.classList.replace('blur-window', 'unblur');
