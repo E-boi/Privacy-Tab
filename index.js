8fb6bd3331e167c6b739609b26bb1063a9feec6a
E-boi <hiboysismyoufake6789@gmail.com>
2021-04-13 17:17:52 -0400

    plugin should even if folder renamed

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { Plugin } = require('powercord/entities');
-const Settings = require("./Components/Settings");
+const Settings = require('./Components/Settings');
 var privacyEnabled = false;
 let keybind;
 
 module.exports = class PrivacyTab extends Plugin {
+	constructor() {
+		super();
+		this.toggler = this.togglePrivacy.bind(this);
+	}
 	startPlugin() {
 		this.loadStylesheet('style.scss');
 		const { get, set } = this.settings;
@@ -15,19 +19,19 @@ module.exports = class PrivacyTab extends Plugin {
 		keybind = get('keybind', 'F6');
 
 		// init
-		document.body.addEventListener("keyup", this.togglePrivacy);
+		document.body.addEventListener('keyup', this.togglePrivacy.bind(this));
 
 		// register settings
 		powercord.api.settings.registerSettings(this.entityID, {
 			category: this.entityID,
 			label: 'Privacy Tab',
-			render: Settings
+			render: Settings,
 		});
 	}
 
 	pluginWillUnload() {
 		powercord.api.settings.unregisterSettings(this.entityID);
-		document.body.removeEventListener("keyup", this.togglePrivacy);
+		document.body.removeEventListener('keyup', this.togglePrivacy.bind(this));
 		if (privacyEnabled) {
 			document.querySelector('.layers-3iHuyZ').classList.remove('blur-window');
 		}
@@ -35,7 +39,7 @@ module.exports = class PrivacyTab extends Plugin {
 
 	// toggle for privacy
 	togglePrivacy(key) {
-		const { getSetting: get } = powercord.api.settings._fluxProps('Privacy-Tab')
+		const { get } = this.settings;
 		const blurElement = document.querySelector('.appMount-3lHmkl');
 		// user safety #1 :P
 		if (!document.hasFocus()) {
@@ -45,17 +49,17 @@ module.exports = class PrivacyTab extends Plugin {
 			privacyEnabled = !privacyEnabled;
 			if (privacyEnabled) {
 				// enable blur
-				let grayscale = "0";
-				if (get("grayscale")) {
-					grayscale = "100%";
+				let grayscale = '0';
+				if (get('grayscale')) {
+					grayscale = '100%';
 				}
-				let interaction = "all";
-				if (get("lock-app")) {
-					interaction = "none";
+				let interaction = 'all';
+				if (get('lock-app')) {
+					interaction = 'none';
 				}
 				let timing = '1s';
-				if (get('blurTiming')) timing = `${get('blurTiming')}s`
-				const blurAmount = get("blur-scale") * 3;
+				if (get('blurTiming')) timing = `${get('blurTiming')}s`;
+				const blurAmount = get('blur-scale') * 3;
 				blurElement.style.setProperty('--blur-window', `blur(${blurAmount}px) grayscale(${grayscale})`);
 				blurElement.style.setProperty('--pointer', `${interaction}`);
 				blurElement.style.setProperty('--blur-timing', `${timing}`);
@@ -63,9 +67,9 @@ module.exports = class PrivacyTab extends Plugin {
 			} else {
 				blurElement.classList.replace('blur-window', 'unblur');
 				setTimeout(() => blurElement.classList.remove('unblur'), 1000);
-			};
-		};
+			}
+		}
 		// if keybind gets changed
-		if (keybind !== get('keybind')) keybind = get('keybind')
+		if (keybind !== get('keybind')) keybind = get('keybind');
 	}
 };
